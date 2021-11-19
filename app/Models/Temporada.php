6a822220fb73b723e33d53d40014d8057f56bcf2
Edeilton Oliveira <edeiltonso@gmail.com>
2021-11-19 11:42:03 -0300

    cria models de temporada e episodio

--- app/Models/Temporada.php
+++ app/Models/Temporada.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Temporada extends Model
 {
-    protected $fillable = ['numero'];
-
     public function episodios()
     {
         return $this->hasMany(Episodio::class);
