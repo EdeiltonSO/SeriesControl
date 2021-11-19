f365c22134ab115de7b1853e95cb6aa46f0c2089
Edeilton Oliveira <edeiltonso@gmail.com>
2021-11-19 11:39:37 -0300

    cria models de temporada e episodio

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -33,11 +33,14 @@ class SeriesController extends Controller
         ]); */
         $serie = Serie::create($req->all());
 
-        //por que nao funciona aqui?
-        //var_dump($serie);
-        //echo $serie;
+        // por que isso nao funciona aqui?
+        // var_dump($serie);
+        // echo "ALO";
+        // echo "<script>console.log('ALO???');</script>";
 
-        //de onde vem esse $serie->id?
+        // de onde vem esse $serie->id, já que ele aparece
+        // mesmo que eu não crie um campo id na migration?
+        // (conferir laravel parte 1, seção 5, session e flash messages)
 
         $req->session()->flash('mensagem', "Série {$serie->nome} com id {$serie->id} adicionada com sucesso");
 
