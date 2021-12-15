f4771f934b1d5b9b81729ba406249f9ed8ce6146
Edeilton Oliveira <edeiltonso@gmail.com>
2021-12-15 16:28:00 -0300

    implementa episodios assistidos e subview para flash message

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Episodio;
 use App\Models\Temporada;
 use Illuminate\Http\Request;
 
 class EpisodiosController extends Controller
 {
     // depende do parâmetro temporada na rota '/temporadas/{temporada}/episodios'
-    public function index(Temporada $temporada)
+    public function index(Temporada $temporada, Request $req)
     {
-        $episodios = $temporada->episodios;
-        return view('episodios.index', compact('episodios'));
+        return view('episodios.index', [
+            'episodios' => $temporada->episodios,
+            'temporadaId' => $temporada->id,
+            'mensagem' => $req->session()->get('mensagem')
+        ]);
+    }
+
+    public function assistir(Temporada $temporada, Request $req)
+    {
+        $episodiosAssistidos = $req->episodios;
+        $temporada->episodios->each(function (Episodio $episodio) use ($episodiosAssistidos) {
+            $episodio->assistido = in_array($episodio->id, $episodiosAssistidos);
+        });
+        $temporada->push();
+
+        $req->session()->flash('mensagem', 'Episódios marcados como assistidos');
+
+        return redirect()->back();
     }
 }
