2401a6ff320c85bf078c9e1e6f422116ff009e55
Edeilton Oliveira <edeiltonso@gmail.com>
2021-12-20 13:05:43 -0300

    implementa barra de navegacao

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,15 @@
     <title>Controle de séries</title>
 </head>
 <body>
+    <nav style="padding: 0px 10px;" class="navbar navbar-expand-lg navbar-dark bg-dark mb-2 d-flex justify-content-between">
+        <a href="{{ route('listar_series') }}" class="navbar-brand">Home</a>
+        @auth
+        <a href="/sair" class="text-danger">Sair</a>
+        @endauth
+        @guest
+        <a href="/entrar">Entrar</a>
+        @endguest
+    </nav>
     <div class="container">
         <div class="jumbotron">
             <h1>@yield('cabecalho')</h1>
